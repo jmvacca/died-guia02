00aee4072a565f5f0673f5d346b3869a70b7a4ad
jmvacca <jmvacca103@gmail.com>
2018-04-12 15:13:42 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,10 @@
+package died.guias.guia02.app;
+
+public class Hola2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("el tiempo actual en milis es: "+System.currentTimeMillis());
+	}
+
+}
